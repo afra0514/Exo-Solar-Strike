e5c555573ce3b5427e7fc9b3e71850efd2765c91
Sumaiya Akter Emo <sumaiyasuhaemu@gmail.com>
2025-12-20 15:23:21 +0600

    Updated utils.h with code

--- codes/utils.h
+++ codes/utils.h
@@ -1,1 +1,14 @@
-
+#ifndef UTILS_H
+#define UTILS_H
+#include "globals.h"
+void drawText(float x, float y, float z, const char* string, void* font = GLUT_BITMAP_HELVETICA_12);
+void drawStrokeText(float x, float y, float z, const char* string, float scale, float r, float g, float b, float lineWidth);
+void drawBackground();
+void drawGlassBox(float x, float y, float w, float h);
+void drawTexturedPlanet(float radius, int textureID);
+void loadAllTextures();
+void initStars();
+void initLighting();
+void playStateMusic(const char* fileName);
+GLuint LoadTexture(const char* filename);
+#endif
