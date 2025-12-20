8a1e22beb4d5c9cd5b0f20ba2bb6be9ae668d177
Sumaiya Akter Emo <sumaiyasuhaemu@gmail.com>
2025-12-20 15:16:48 +0600

    pdated globals.h with code

--- codes/globals.h
+++ codes/globals.h
@@ -1,1 +1,16 @@
 
+#ifndef GLOBALS_H
+#define GLOBALS_H
+#include "definitions.h"  
+extern int WIDTH;
+extern int HEIGHT; 
+extern State currentState; 
+extern float solarAngle, moonAngle, cameraZoom, infoZoom;
+extern float camX, camY; 
+extern float shipX, shipY;
+extern float asteroidX[5], asteroidY[5], asteroidRot[5], asteroidScale[5];
+extern int score, difficultyLevel, currentInfoIndex;
+extern bool gameRunning;
+extern Star stars[STAR_COUNT];
+extern GLuint planetTextures[15];
+#endif
